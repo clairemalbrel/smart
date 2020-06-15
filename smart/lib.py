8ca8ae2347c7b291b1e413561bb443f8b1148ad9
Claire Malbrel <clairemalbrel@gmail.com>
2020-06-15 15:45:48 +0200

    test

--- smart/lib.py
+++ smart/lib.py
@@ -47,6 +47,8 @@ def clean_data(data):
     data.loc[:, 'Frequency'] = data['Frequency'].map(drows)
     return data
 
+def coucou():
+    print('coucou les amigos')
 
 if __name__ == '__main__':
     # For introspections purpose to quickly get this functions on ipython
